f35dd10f18408c4b48d1a423715987c7116a9717
Agung Kurniawan <agung.kurniawan1894@gmail.com>
2022-06-12 15:03:46 +0700

    Challenge 07 Agung Kurniawan (152235865101-374)

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (stringFilter) => {
+  try {
+    const [resultIXX, resultVGC] = await Promise.all([
+                                      promiseTheaterIXX(),
+                                      promiseTheaterVGC(),
+                                   ]);
+    let sum = 0;
+    sum += resultIXX?.filter((item) => item.hasil === stringFilter)?.length || 0;
+    sum += resultVGC?.filter((item) => item.hasil === stringFilter)?.length || 0;
+    return sum;
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+};
 
 module.exports = {
   promiseOutput,
